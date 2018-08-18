a61ae9f9c3a67f9c0f8536c12425a94e580b8040
Quanynawy <42499386+Quanynawy@users.noreply.github.com>
2018-08-18 22:40:18 +0300

    aa
    aa

---
+++ src/main/java/ru/roccommod/worstmodever/WorstModEver.java
@@ -0,0 +1,32 @@
+package ru.roccommod.worstmodever;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import ru.roccommod.worstmodever.lib.References;
+
+@Mod(modid = References.MODID, name  = References.MODNAME, version = References.VERSION)
+
+public class WorstModEver 
+{
+	
+	@EventHandler
+	public void preLoad(FMLPreInitializationEvent event)
+	{
+	    
+	}
+	
+	@EventHandler
+	public void load(FMLInitializationEvent event)
+	{
+	    
+	}
+	
+	@EventHandler
+	public void postLoad(FMLPostInitializationEvent event)
+	{
+	    
+	}
+}
