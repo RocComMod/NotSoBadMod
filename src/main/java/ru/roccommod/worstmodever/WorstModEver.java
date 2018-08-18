7797aaf52e4202abf9f4e331169e2319d1a80a15
Quanynawy <42499386+Quanynawy@users.noreply.github.com>
2018-08-18 22:19:14 +0300

    Add files via upload

---
+++ src/main/java/ru/roccommod/worstmodever/WorstModEver.java
@@ -0,0 +1,12 @@
+package ru.roccommod.worstmodever;
+
+import cpw.mods.fml.common.Mod;
+import ru.roccommod.worstmodever.lib.References;
+
+@Mod(modid = References.MODID, name  = References.MODNAME, version = References.VERSION)
+
+public class WorstModEver 
+{
+	
+	
+}
---
+++ src/main/java/ru/roccommod/worstmodever/lib/References.java
@@ -0,0 +1,9 @@
+package ru.roccommod.worstmodever.lib;
+
+public class References {
+	
+	public static final String MODID = "wme";
+	public static final String MODNAME = "WorstModEver";
+	public static final String VERSION = "0.1.0";
+
+}
